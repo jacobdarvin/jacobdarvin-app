15f6b6d21793a262b46414e0dddadf3f32b7f3af
Jacob Darvin <jacobisdarvin@gmail.com>
2024-10-18 12:09:51 +0800

    jacobdarvin v2, 2024 initial commit

---
+++ app/components/Pill.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaLink,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+type PillProps = {
+  href: string;
+  label: string;
+  handle: string;
+};
+
+const Pill: React.FC<PillProps> = ({ href, label, handle }) => {
+  const getLogo = (label: string) => {
+    switch (label) {
+      case "GitHub":
+        return <FaGithub />;
+      case "LinkedIn":
+        return <FaLinkedin />;
+      case "X":
+        return <FaXTwitter />;
+      case "Email":
+        return <FaEnvelope />;
+      case "Facebook":
+        return <FaFacebook />;
+      case "Instagram":
+        return <FaInstagram />;
+      case "Link":
+        return <FaLink />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <a
+      href={href}
+      className="flex items-center gap-2 px-4 py-2 border rounded-full hover:bg-neutral-100 dark:hover:bg-neutral-900 transition text-sm font-medium"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {getLogo(label)}
+      {handle}
+    </a>
+  );
+};
+
+export default Pill;
